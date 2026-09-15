2e01ffcc70d70c61c0dbd61eb4c2f1230f9d0d37
tetourne <thomas.etourneau@tutanota.com>
2026-09-16 01:33:00 +0200

    Adding filters to select data

--- data_visualisation.py
+++ data_visualisation.py
@@ -6,6 +6,7 @@ import sklearn as skl
 
 # Loading data
 data_dir = '/run/media/thomas/Data/Documents/Database'
+print(f"Reading data from {data_dir}...")
 audio_dir = data_dir+'/fma_small'
 tracks = utils.load(data_dir+'/fma_metadata/tracks.csv')
 genres = utils.load(data_dir+'/fma_metadata/genres.csv')
@@ -13,12 +14,20 @@ features = utils.load(data_dir+'/fma_metadata/features.csv')
 echonest = utils.load(data_dir+'/fma_metadata/echonest.csv')
 np.testing.assert_array_equal(features.index, tracks.index)
 assert echonest.index.isin(tracks.index).all()
+print("Data loaded correctly.")
 
 # Selecting data sets
-msk = [tracks['set', 'subset'] <= 'small']
-tracks = tracks[msk]
+dataset_name = "small"
+print(f"\n\nSelecting data set: {dataset_name}...")
+smallset_ids = tracks['set', 'subset'] <= dataset_name
+tracks = tracks[smallset_ids]
+features = features[smallset_ids]
+print("Done selecting.")
 
-# First insights
-print(tracks.head())
-print(tracks.info())
-print(tracks.describe())
+print("\n\nSelecting training, validating and test sets...")
+trainingset_ids = tracks['set', 'split'] == 'training'
+tracks = tracks[trainingset_ids]
+features = features[trainingset_ids]
+print("Done selecting.")
+
+
